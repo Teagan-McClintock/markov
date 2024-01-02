5191c8cac4776a41838472f42d1419462eee5bd1
Nick Monteleone <nicholasmonteleone25@gmail.com>
2024-01-02 15:56:54 -0800

    added get text function

--- markov.js
+++ markov.js
@@ -1,5 +1,7 @@
 /** Textual markov chain generator. */
 
+const _ = require("lodash");
+
 
 class MarkovMachine {
 
@@ -31,28 +33,33 @@ class MarkovMachine {
 
     const chains = {};
 
-    for (let i = 0; i < self.words.length; i++) {
-      const currChain = chains[self.words[i]] || [];
-      currChain.push(self.words[i + 1] || null);
-      chains[self.words[i]] = currChain;
+    for (let i = 0; i < this.words.length; i++) {
+      const currChain = chains[this.words[i]] || [];
+      currChain.push(this.words[i + 1] || null);
+      chains[this.words[i]] = currChain;
     }
 
     return chains;
   }
 
-
   /** Return random text from chains, starting at the first word and continuing
    *  until it hits a null choice. */
 
   getText() {
-    // TODO: implement this!
-
     // - start at the first word in the input text
     // - find a random word from the following-words of that
     // - repeat until reaching the terminal null
+
+    let wordToAdd = this.words[0];
+    let outputText = '';
+
+    while (wordToAdd !== null) {
+      outputText += `${wordToAdd} `;
+      wordToAdd = _.sample(this.chains[wordToAdd]);
+    }
+
+    return outputText.trimEnd();
   }
 }
 
 module.exports = { MarkovMachine };
-
-//TODO: Make a random number generator or even better just use lodash
